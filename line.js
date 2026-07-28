51a8faf1600dcff001eefcf7d8cf5e0b9e2588c8
iki1uc <entertrainer@web.de>
2026-07-28 11:49:14 +0200

    Update line.js

--- line.js
+++ line.js
@@ -7,3 +7,21 @@ Rechner 2:
   Q81
   TMP81
   transport.check(u)
+pipeline.hw = {
+    station1: {
+        name: "GPU",
+        competence: "engine.power"
+    },
+    station2: {
+        name: "CPU",
+        competence: "engine.logic"
+    },
+    station3: {
+        name: "RAM",
+        competence: "engine.memory"
+    },
+    station4: {
+        name: "ROM",
+        competence: "81.tmp"
+    }
+};
